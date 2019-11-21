345b843efff18386b4e8ca38d524e4f4eb9b0d98
HS <sekhah@my.lanecc.edu>
2019-11-21 07:40:42 -0800

    More files and NuGet packages.

---
+++ ComputerInventory/Models/MachineType.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+
+namespace ComputerInventory.Models
+{
+    public partial class MachineType
+    {
+        public MachineType()
+        {
+            Machine = new HashSet<Machine>();
+        }
+        public int MachineTypeId { get; set; }
+        public string Description { get; set; }
+        public ICollection<Machine> Machine { get; set; }
+    }
+}
